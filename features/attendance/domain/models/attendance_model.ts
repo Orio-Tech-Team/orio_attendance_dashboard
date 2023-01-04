c67c6fa25a2faa2096e38623f4487becc55b6f77
Muhammad Ismail <itsmuhammadismail@gmail.com>
2023-01-04 16:05:25 +0500

    time updated

--- features/attendance/domain/models/attendance_model.ts
+++ features/attendance/domain/models/attendance_model.ts
@@ -12,9 +12,10 @@ class AttendanceModel {
     this.employeeNumber = json.employee_number;
     this.employeeName = json.employee_name;
     this.attendanceDate = json.attendance_date;
-    this.inTime = json.intime != 0 ? json.intime : "00:00";
-    this.outTime = json.outtime != 0 ? json.outtime : "00:00";
-    this.workingHour = json.working_hours != 0 ? json.working_hours : "00:00";
+    this.inTime = json.intime != 0 ? json.intime.substring(0, 5) : "00:00";
+    this.outTime = json.outtime != 0 ? json.outtime.substring(0, 5) : "00:00";
+    this.workingHour =
+      json.working_hours != 0 ? json.working_hours.substring(0, 5) : "00:00";
     this.type = json.type;
     this.shift = json.shift;
   }
